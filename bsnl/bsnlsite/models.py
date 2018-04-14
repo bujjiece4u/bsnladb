2afc6e5855e8da4eb69f21813f6d83f33db08d76
Juluri <suresh.babu.juluri@intel.com>
2018-04-14 23:32:35 +0530

    DB created

--- bsnl/bsnlsite/models.py
+++ bsnl/bsnlsite/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class bsnlsitedb(models.Model):
+    sitename = models.CharField(max_length=200)
+    btstype  = models.CharField(max_length=8)
+    btsmake  = models.CharField(max_length=200)
+    battery  = models.CharField(max_length=5)
+    powerplant = models.CharField(max_length=4)
+    ac         = models.CharField(max_length=2)
