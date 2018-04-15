97f4a65524ae2a1fb72c28010505d0b9b625eb1b
Juluri <suresh.babu.juluri@intel.com>
2018-04-15 07:04:11 +0530

    Basic view is working listing all databases; Need to move this view to list/results

--- bsnl/bsnlsite/urls.py
+++ bsnl/bsnlsite/urls.py
@@ -4,4 +4,10 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    # ex: /bsnlsite/5/
+    path('<int:bsnlsitedb_id>/', views.detail, name='detail'),
+    # ex: /polls/5/results/
+    path('<int:bsnlsitedb_id>/results/', views.results, name='results'),
+    # ex: /polls/5/vote/
+    path('<int:bsnlsitedb_id>/entry/', views.entry, name='entry'),    
 ]
--- bsnl/bsnlsite/views.py
+++ bsnl/bsnlsite/views.py
@@ -1,5 +1,21 @@
 from django.http import HttpResponse
 
+from .models import bsnlsitedb
 
+#def index(request):
+#    return HttpResponse("Welcome to BSNL site for entry form.")
 def index(request):
-    return HttpResponse("Welcome to BSNL site for entry form.")
+    latest_bsnlsitedb_list = bsnlsitedb.objects.all()
+    output = ', '.join([q.sitename for q in latest_bsnlsitedb_list])
+    return HttpResponse(output)
+
+
+def detail(request, bsnlsitedb_id):
+    return HttpResponse("You're looking at question %s." % bsnlsitedb_id)
+
+def results(request, bsnlsitedb_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % bsnlsitedb_id)
+
+def entry(request, bsnlsitedb_id):
+    return HttpResponse("You're voting on question %s." % bsnlsite_db)
