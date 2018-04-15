9dc7619ef08605344c989386ca878b53edad8c95
Juluri <suresh.babu.juluri@intel.com>
2018-04-15 21:06:58 +0530

    AC types added till 10

--- bsnl/bsnlsite/forms.py
+++ bsnl/bsnlsite/forms.py
@@ -6,4 +6,19 @@ class PostForm(forms.ModelForm):
 
     class Meta:
         model = bsnlsitedb
-        fields = ('sitename', 'btstype','btsmake',)
+        fields = ('sitename',
+                  'btstype',
+                  'btsmake',
+                  'battery',
+                  'batterytype',
+                  'powerplant',
+                  'powerplantmodule',
+                  'ac1make',
+                  'ac1capacity',
+                  'ac2make',
+                  'ac2capacity',
+                  'ac3make',
+                  'ac3capacity',
+                  'ac4make',
+                  'ac4capacity',
+                  )
--- bsnl/bsnlsite/models.py
+++ bsnl/bsnlsite/models.py
@@ -10,10 +10,41 @@ BTSTYPE_CHOICES = (
 ('te+2g+3g', 'TE+2G+3G'),
 
 )
+
+BATTERYTYPE_CHOICES =(
+('100ah', '100AH'),
+('200ah', '200AH'),
+('300ah', '300AH'),
+)
+POWERPLANTMODULE_CHOICES = (
+('50a', '50A'),
+('10a', '10A'),
+('25a', '25A'),
+('100a', '100A'),
+)
+
+ACCAPACITY_CHOICES =(
+('1t', '1T'),
+('1.5t', '1.5T'),
+('2t', '2T'),
+('2.5t', '2.5T'),
+
+)
 class bsnlsitedb(models.Model):
     sitename = models.CharField(max_length=200)
     btstype  = models.CharField(max_length=8,choices=BTSTYPE_CHOICES,default='2G')
     btsmake  = models.CharField(max_length=200)
-    battery  = models.CharField(max_length=5)
-    powerplant = models.CharField(max_length=4)
-    ac         = models.CharField(max_length=2)
+    battery  = models.CharField(max_length=200)
+    batterytype = models.CharField(max_length=5,choices=BATTERYTYPE_CHOICES,default='100AH')
+    powerplant = models.CharField(max_length=200)
+    powerplantmodule = models.CharField(max_length=4, choices=POWERPLANTMODULE_CHOICES,default='50A')
+    ac1make         = models.CharField(max_length=200)
+    ac1capacity         = models.CharField(max_length=4,choices=ACCAPACITY_CHOICES,default='1T')
+    ac2make         = models.CharField(max_length=200)
+    ac2capacity         = models.CharField(max_length=4,choices=ACCAPACITY_CHOICES,default='1T')
+    ac3make         = models.CharField(max_length=200)
+    ac3capacity         = models.CharField(max_length=4,choices=ACCAPACITY_CHOICES,default='1T')
+    ac4make         = models.CharField(max_length=200)
+    ac4capacity         = models.CharField(max_length=4,choices=ACCAPACITY_CHOICES,default='1T')
+    ac5make         = models.CharField(max_length=200)
+    ac5capacity         = models.CharField(max_length=4,choices=ACCAPACITY_CHOICES,default='1T')
