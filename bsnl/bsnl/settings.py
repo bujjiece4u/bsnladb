87fa7b9c991015746d7375781e15fdd124cf380c
Juluri <suresh.babu.juluri@intel.com>
2018-04-28 16:48:29 +0530

    Table in index is proper TODO: need to et border

--- bsnl/bsnl/settings.py
+++ bsnl/bsnl/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     #Added by Suresh
-    'haystack'
+    'haystack',
+    'django_tables2',
 ]
 
 HAYSTACK_CONNECTIONS = {
@@ -71,6 +72,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.request',
             ],
         },
     },
